8869067a5866fdcd7a999704e10abeafa0470e82
Thai Thanh Kha <khack21v7k012@vlvh.ctu.edu.vn>
2022-12-01 13:59:53 +0700

    Xay dung trang cac san pham nguoi dung

---
+++ lib/ui/products/user_product_list_tile.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
+import '../screen.dart';
+import '../../models/product.dart';
+
+class UserProductListTile extends StatelessWidget {
+  final Product product;
+
+  const UserProductListTile(
+    this.product, {
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(product.title),
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(product.imageUrl),
+      ),
+      trailing: SizedBox(
+        width: 100,
+        child: Row(
+          children: <Widget>[
+            buildEditButton(context),
+            buildDeleteButton(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildDeleteButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.delete),
+      onPressed: () {
+        context.read<ProductsManager>().deleteProduct(product.id!);
+        ScaffoldMessenger.of(context)
+          ..hideCurrentSnackBar()
+          ..showSnackBar(
+            const SnackBar(
+              content: Text(
+                'Product deleted',
+                textAlign: TextAlign.center,
+              ),
+            ),
+          );
+      },
+      color: Theme.of(context).errorColor,
+    );
+  }
+
+  Widget buildEditButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.edit),
+      onPressed: () {
+        Navigator.of(context).pushNamed(
+          EditProductsScreen.routeName,
+          arguments: product.id,
+        );
+      },
+      color: Theme.of(context).primaryColor,
+    );
+  }
+
+ 
+}
---
+++ lib/ui/products/user_products_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'user_product_list_tile.dart';
+import 'products_manager.dart';
+import '../shared/app_drawer.dart';
+import 'package:provider/provider.dart';
+import './edit_product_screen.dart';
+
+class UserProductsScreen extends StatelessWidget {
+  static const routeName = '/user-products';
+  const UserProductsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final productsManager = ProductsManager();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Products'),
+        actions: <Widget>[
+          buildAddButton(context),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: RefreshIndicator(
+        onRefresh: () async => print('refresh products'),
+        child: buildUserProductListView(productsManager),
+      ),
+    );
+  }
+
+  Widget buildUserProductListView(ProductsManager productsManager) {
+    return Consumer<ProductsManager>(
+      builder: (ctx, productsManager, child) {
+        return ListView.builder(
+          itemCount: productsManager.itemCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              UserProductListTile(
+                productsManager.items[i],
+              ),
+              const Divider(),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildAddButton(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.add),
+      onPressed: () {
+        Navigator.of(context).pushNamed(
+          EditProductsScreen.routeName,
+        );
+      },
+    );
+  }
+}
