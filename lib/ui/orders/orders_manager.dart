fe8b36a8fddf8491bb79ef9f42578fbdbf44bca5
Thai Thanh Kha <khack21v7k012@vlvh.ctu.edu.vn>
2022-12-01 14:05:09 +0700

    Xay dung trang cac dat hang

---
+++ lib/ui/orders/orders_manager.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
+import '../../models/cart_item.dart';
+import '../../models/order_item.dart';
+class OrdersManager with ChangeNotifier{
+  final List<OrderItem> _orders = [
+    OrderItem(
+      id: 'o1',
+      amount: 59.98,
+      products: [
+        CartItem(
+          id: 'c1',
+          title: 'Red Shirt',
+          price: 29.99,
+          quantity: 2,
+        )
+      ],
+      dateTime: DateTime.now(),
+    )
+  ];
+
+  int get orderCount {
+    return _orders.length;
+  }
+
+  List<OrderItem> get orders {
+    return [..._orders];
+  }
+  void addOrder(List<CartItem> cartProducts, double total) async{
+    _orders.insert(
+      0,
+      OrderItem(
+        id: 'o${DateTime.now().toIso8601String()}',
+        amount: total,
+        products: cartProducts,
+        dateTime: DateTime.now(),
+      ),
+    );
+    notifyListeners();
+  }
+}
---
+++ lib/ui/orders/orders_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import '../shared/app_drawer.dart';
+import 'orders_manager.dart';
+import 'order_item_card.dart';
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
+
+class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
+  const OrdersScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    print('building orders');
+    final ordersManager = OrdersManager();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Orders"),
+      ),
+      drawer: const AppDrawer(),
+      body: Consumer<OrdersManager>(
+        builder: (ctx, ordersManager, child) {
+          return ListView.builder(
+            itemCount: ordersManager.orderCount,
+            itemBuilder: (ctx, i) => OrderItemCard(ordersManager.orders[i]),
+          );
+        },
+      ),
+    );
+  }
+}
