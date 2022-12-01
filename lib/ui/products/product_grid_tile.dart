79d30159074b7b2777861169ca87a776e1022015
Thai Thanh Kha <khack21v7k012@vlvh.ctu.edu.vn>
2022-12-01 13:58:57 +0700

    Xay dung trang tong quan cac san pham

---
+++ lib/ui/products/product_grid_tile.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import '../../models/product.dart';
+import 'product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import '../screen.dart';
+
+class ProductGridTile extends StatelessWidget {
+  const ProductGridTile(
+    this.product, {
+    super.key,
+  });
+
+  final Product product;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: buildGridFooterBar(context),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(
+              ProductDetailScreen.routeName,
+              arguments: product.id,
+            );
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildGridFooterBar(BuildContext context) {
+    return GridTileBar(
+      backgroundColor: Colors.black87,
+      leading: ValueListenableBuilder<bool>(
+        valueListenable: product.isFavoriteListenable,
+        builder: (ctx, isFavorite, child) {
+          return IconButton(
+            icon: Icon(
+              isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
+            color: Theme.of(context).colorScheme.secondary,
+            onPressed: () {
+              final cart = context.read<CartManager>();
+              cart.addItem(product);
+              ScaffoldMessenger.of(context)
+                ..hideCurrentSnackBar()
+                ..showSnackBar(
+                  SnackBar(
+                      content: const Text(
+                        'Item added to cart',
+                      ),
+                      duration: const Duration(seconds: 2),
+                      action: SnackBarAction(
+                        label: 'UNDO',
+                        onPressed: () {
+                          cart.removeSingleItem(product.id!);
+                        },
+                      )),
+                );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ lib/ui/products/product_overview_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'products_grid.dart';
+import '../shared/app_drawer.dart';
+import '../screen.dart';
+import 'top_right_badge.dart';
+import '../screen.dart';
+import 'package:provider/provider.dart';
+
+enum FilterOptions { favorite, all }
+
+class ProductsOverviewScreen extends StatefulWidget {
+  const ProductsOverviewScreen({super.key});
+
+  @override
+  State<ProductsOverviewScreen> createState() => _ProductOverviewScreenState();
+}
+
+class _ProductOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('MyShop'),
+        actions: <Widget>[
+          buildProductFilterMenu(),
+          buildShoppingCartIcon(),
+        ],
+      ),
+      drawer: const AppDrawer(),
+      body: ProductsGrid(_showOnlyFavorites),
+    );
+  }
+
+  Widget buildShoppingCartIcon() {
+    return Consumer<CartManager>(
+      builder: (ctx, cartManager, child) {
+        return TopRightBadge(
+          data: cartManager.productCount,
+          child: IconButton(
+            icon: const Icon(
+              Icons.shopping_cart,
+            ),
+            onPressed: () {
+              Navigator.of(ctx).pushNamed(CartScreen.routeName);
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  Widget buildProductFilterMenu() {
+    return PopupMenuButton(
+      onSelected: (FilterOptions selectedValue) {
+        setState(() {
+          if (selectedValue == FilterOptions.favorite) {
+            _showOnlyFavorites = true;
+          } else {
+            _showOnlyFavorites = false;
+          }
+        });
+      },
+      icon: const Icon(
+        Icons.more_vert,
+      ),
+      itemBuilder: (ctx) => [
+        const PopupMenuItem(
+          value: FilterOptions.favorite,
+          child: Text('Only Favorites'),
+        ),
+        const PopupMenuItem(
+          value: FilterOptions.all,
+          child: Text('Show All'),
+        ),
+      ],
+    );
+  }
+}
